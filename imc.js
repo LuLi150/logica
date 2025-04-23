ed3cd00513241d6e3e69249db7b33923c6df1679
Geração Tech <geracaotech@digitalcollege.internal>
2025-04-23 16:51:28 -0300

    imc

---
+++ imc.js
@@ -0,0 +1,17 @@
+ const readline = require("readline-sync")
+
+let peso=parseInt(readline.question("qual seu peso?"));
+let altura=parseInt(readline.question("Qual a sua altura?"));
+let imc=peso/(altura*altura)
+
+if(imc<18){
+    console.log("muito abaixo do peso!")
+} else if(imc>=25){
+    console.log("peso normal!")
+} else if(imc>=39){
+    console.log("acima do peso!")
+} else {
+    console.log("muito acima do peso!!")
+}
+
+
